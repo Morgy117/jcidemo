38e7cea3a05ee05cade36a68729a5155be8a3599
Morgy117 <cmorgan47@qub.ac.uk>
2020-10-15 14:08:04 +0100

    Reset

--- src/uk/ac/qub/eeecs/dave/CalculatorTest.java
+++ src/uk/ac/qub/eeecs/dave/CalculatorTest.java
@@ -1,20 +1,18 @@
 package uk.ac.qub.eeecs.dave;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class CalculatorTest {
 
 	@Test
 	void testAdd() {
 		Calculator c = new Calculator();
-		assert 1==2;
+		assertEquals(c.add(10,1), 11);
 	}
 
 	private void assertEquals(int add, int i) {
 	}
 
-	////
-	////
 	@Test
 	void testSub() {
 		Calculator c = new Calculator();
